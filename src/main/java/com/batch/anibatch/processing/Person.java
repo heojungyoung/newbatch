6900a5d5ee31a0ab02e9bca07842b107203e03ab
heojungyoung <tyireal@gmail.com>
2024-09-03 21:22:51 +0900

    init

---
+++ src/main/java/com/batch/anibatch/processing/Person.java
@@ -0,0 +1,5 @@
+package com.batch.anibatch.processing;
+
+public record Person(String firstName, String lastName) {
+
+}
---
+++ src/test/java/com/batch/anibatch/AnibatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.anibatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnibatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
